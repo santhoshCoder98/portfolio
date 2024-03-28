807280a74fe0536ae3371b353258827b5f4ee599
santhoshCoder98 <santhosh98720@gmail.com>
2024-03-28 12:23:47 -0700

    Code Commit

---
+++ src/components/Experience.js
@@ -0,0 +1,87 @@
+import React, { useRef } from "react";
+import {
+  motion,
+  useScroll,
+} from "framer-motion";
+import LiIcon from "./LiIcon";
+
+
+const Details = ({ position, company, companyLink, time, address, work }) => {
+  const ref = useRef(null);
+  return (
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]"
+    >
+      <LiIcon reference={ref} />
+      <motion.div
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
+      >
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+          {position}{" "}
+          <a
+            className="capitalize text-primary dark:text-primaryDark"
+            href={companyLink}
+            target={"_blank"}
+          >
+            @{company}
+          </a>
+        </h3>
+        <span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
+          {time} | {address}
+        </span>
+        <p className="font-medium w-full md:text-sm"> {work}</p>
+      </motion.div>
+    </li>
+  );
+};
+
+const Experience = () => {
+
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
+
+  return (
+
+      <div className="my-64">
+        <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+          Experience
+        </h2>
+
+        <div ref={ref} className="relative w-[75%] mx-auto lg:w-[90%] md:w-full">
+          <motion.div
+            className="absolute left-9 top-0 w-[4px] md:w-[2px] md:left-[30px] xs:left-[20px] h-full bg-dark 
+            origin-top  dark:bg-primaryDark dark:shadow-3xl"
+            style={{ scaleY: scrollYProgress }}
+          />
+          <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
+            <Details
+              position="Senior Software Engineer"
+              company="Aspire Systems"
+              companyLink=""
+              time="2020-2023"
+              address="Chennai, India"
+              work="As a Senior Software Engineer at Aspire Systems, I led the development of 10+ modules, achieving a 75% reduction in transaction search latency and earning the Role Star Award. I collaborated with stakeholders, increasing client satisfaction by 35%, and implemented agile methodologies for a 20% improvement in project efficiency. My strategic establishment of a CI/CD pipeline using Azure DevOps resulted in a 40% reduction in release time. Proactively resolving over 100 bugs, I improved application functionality by 30% and decreased transaction search access time from 20 seconds to less than 5 seconds. My impactful contributions showcase a commitment to successful outcomes and continuous improvement at Aspire Systems."
+            />
+
+            <Details
+              position="Software Engineer"
+              company="Temenos"
+              companyLink=""
+              time="2021-2022"
+              address="Hyderabad, India"
+              work="During my role as a Software Engineer at Temenos, I focused on optimizing the Settings page, achieving a 15% improvement in system performance, and enhancing critical UI functionalities, leading to a 25% increase in user engagement. Addressing and resolving over 10 issues in service middleware, I significantly enhanced the overall application stability. Collaborating across departments, I introduced continuous enhancement strategies, resulting in a 15% decrease in testing cycles. My contributions at Temenos underscore my commitment to refining user experiences, optimizing system functionality, and implementing strategic improvements in a collaborative and dynamic environment."
+            />
+          </ul>
+        </div>
+        </div>
+    );
+};
+
+export default Experience;
---
+++ src/pages/about.js
@@ -0,0 +1,136 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import profile from "../../public/images/profile/developer-pic-4.png";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
+import AnimatedText from "@/components/AnimatedText";
+import TransitionEffect from "@/components/TransitionEffect";
+
+function AnimatedNumberFramerMotion({ value }) {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [motionValue, value, isInView]);
+
+  useEffect(
+    () =>
+      springValue.on("change", (latest) => {
+        if (ref.current && latest.toFixed(0) <= value) {
+          ref.current.textContent = latest.toFixed(0);
+        }
+      }),
+    [springValue, value]
+  );
+
+  return <span ref={ref} />;
+}
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Minimal Portfolio Built with Nextjs | About Page</title>
+        <meta name="description" content="Learn more about CodeBucks, a Next.js developer with a passion for 
+        creating innovative solutions. Discover tips for building a developer portfolio and insights on 
+        full-stack development, front-end development, and back-end development." />
+      </Head>
+      <TransitionEffect />
+      <main
+        className={`flex  w-full flex-col items-center justify-center dark:text-light`}
+      >
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Fervor Fuels Meaningful Action!"
+            className="mb-16 !text-8xl !leading-tight lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
+          />
+
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 
+            md:col-span-8">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
+                BIOGRAPHY
+              </h2>
+              <p className="font-medium ">
+                Hi, I&apos;m <strong>Santhosh Chandrasekaran</strong>, a passionate Software Engineer and Frontend Developer currently pursuing a Master's in Computer Science at the University of California, Irvine. With a strong academic background and a GPA of 4.0/4.0, I specialize in areas such as Data Structures and Algorithms, Machine Learning, and Artificial Intelligence.
+              </p>
+              <p className="my-4 font-medium">
+                Outside my academic pursuits, I've dedicated personal time to interesting projects that showcase my technical skills. CulinaryCrafts, a user-friendly web application developed using React, allows users to add, update, and store recipes. The project demonstrates my proficiency in HTML5, CSS3, JavaScript, Bootstrap, and React. Another project, Forkify, is a Recipe Management Web App developed with HTML5, CSS3, and JavaScript, elevating the UI by 40% with Material UI. I seamlessly integrated the Forkify API, enabling efficient searching, viewing, and bookmarking of recipes.
+              </p>
+              <p className="font-medium">
+                In addition to my personal projects, my professional journey includes a role as a Senior Software Engineer at Aspire Systems, where I led the development of impactful modules, significantly reducing transaction search latency and earning the Role Star Award for my contributions. I've successfully deployed CI/CD pipelines using Azure DevOps, resulting in a substantial reduction in release time.
+              </p>
+
+              <p className="font-medium mt-4">
+              My expertise spans software development, frontend technologies, CI/CD, UI/UX design, and more. As an individual committed to continuous improvement, I am actively seeking opportunities to collaborate and enhance my skills. Let's connect to explore how my diverse background and skills can contribute to your team's success. Feel free to reach out via email at santhoc@uci.edu or connect with me on LinkedIn and GitHub. Looking forward to exciting opportunities and meaningful collaborations!
+              </p>
+            </div>
+            <div className="relative col-span-3 h-max rounded-2xl border-2 border-solid border-dark 
+            bg-light p-8 dark:border-light dark:bg-dark
+            xl:col-span-4 md:col-span-8 md:order-1
+            ">
+              <div
+                className="absolute  top-0 -right-3 -z-10 h-[103%] w-[102%]  rounded-[2rem] rounded-br-3xl 
+                bg-dark
+        dark:bg-light  "
+              />
+              <Image
+                className="h-[700px] w-full rounded-2xl"
+                src={profile}
+                alt="Codebucks"
+                sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+                priority
+              />
+            </div>
+            <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row 
+            xl:items-center md:order-3">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumberFramerMotion value={2} />+
+                </span>
+                <h2 className="mb-4 text-xl font-medium capitalize text-dark/75 dark:text-light/75 
+                xl:text-center md:text-lg sm:text-base xs:text-sm">
+                  satisfied clients
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumberFramerMotion value={10} />+
+                </span>
+                <h2 className="mb-4 text-xl font-medium capitalize text-dark/75 dark:text-light/75 
+                xl:text-center md:text-lg sm:text-base xs:text-sm">
+                  projects completed
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumberFramerMotion value={2} />+
+                </span>
+                <h2 className="mb-4 text-xl font-medium capitalize text-dark/75 dark:text-light/75 
+                xl:text-center md:text-lg sm:text-base xs:text-sm">
+                  Years of experience
+                </h2>
+              </div>
+            </div>
+          </div>
+
+          <Skills />
+          <Experience />
+          <Education />
+        </Layout>
+      </main>
+    </>
+  );
+}
