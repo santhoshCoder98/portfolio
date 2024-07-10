26682f74a22a8a3b74d4c06ca8c81402020ccc4e
Santhosh Chandrasekaran <42298132+santhoshCoder98@users.noreply.github.com>
2024-07-10 10:02:28 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Awesome Portfolio Built with Nextjs</title>
+        <title>Santhosh Chandrasekaran</title>
         <meta
           name="description"
           content="Portfolio website of a full-stack developer. Explore my latest projects and articles, showcasing my expertise in React.js and web development."
